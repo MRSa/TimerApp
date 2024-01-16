14963e10137cc03c5b2b8cbd00bfaba1afbb4559
MRSa <mrsa@myad.jp>
2024-01-16 23:37:16 +0900

    ボタンの処理変更2

--- app/src/main/java/net/osdn/ja/gokigen/wearos/timerapp/presentation/ui/MainControlButtons.kt
+++ app/src/main/java/net/osdn/ja/gokigen/wearos/timerapp/presentation/ui/MainControlButtons.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
